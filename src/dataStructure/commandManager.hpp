7d17d35c2ebc26f1a120697e30de161c734c79d8
tianer2820 <tianer2820@163.com>
2020-06-22 16:19:24 +0800

    commandManager::create now return proper values

--- src/dataStructure/commandManager.hpp
+++ src/dataStructure/commandManager.hpp
@@ -4,30 +4,39 @@
 #include <list>
 #include "command.hpp"
 
-class CommandManager{
-    public:
-    static CommandManager* create(){
-        if(instance_count > 0){
+class CommandManager
+{
+public:
+    static CommandManager *create()
+    {
+        if (instance_count > 0)
+        {
             return instance_ptr;
         }
         instance_ptr = new CommandManager();
         instance_count += 1;
+        return instance_ptr;
     }
-    static void destruct(){
-        if(instance_count > 0){
+    static void destruct()
+    {
+        if (instance_count > 0)
+        {
             delete instance_ptr;
             instance_count = 0;
         }
     }
-    static CommandManager* getInstance(){
-        if(instance_count == 0){
+    static CommandManager *getInstance()
+    {
+        if (instance_count == 0)
+        {
             // CommandManager::create();
             throw "Command manager is not created";
         }
         return instance_ptr;
     }
 
-    void add(Command* command){
+    void add(Command *command)
+    {
         // empty the later command
         while (next_command != command_list.end())
         {
@@ -38,23 +47,28 @@ class CommandManager{
         command_list.push_back(command);
         next_command = command_list.end();
 
-        if(command_list.size() > largest_num){
+        if (command_list.size() > largest_num)
+        {
             delete *(command_list.begin()); // delete the command object
-            command_list.pop_front(); // limit the command list size
+            command_list.pop_front();       // limit the command list size
         }
     }
 
-    bool undo(){
-        if(next_command == command_list.begin()){
+    bool undo()
+    {
+        if (next_command == command_list.begin())
+        {
             // no command to undo
             return false;
         }
-        next_command --;
+        next_command--;
         (*next_command)->undo();
         return true;
     }
-    bool redo(){
-        if(next_command == command_list.end()){
+    bool redo()
+    {
+        if (next_command == command_list.end())
+        {
             //no command to redo
             return false;
         }
@@ -63,25 +77,28 @@ class CommandManager{
         return true;
     }
 
-    protected:
+protected:
     static int instance_count;
-    static CommandManager* instance_ptr;
+    static CommandManager *instance_ptr;
     static const int largest_num = 32;
 
-    list<Command*> command_list;
-    list<Command*>::iterator next_command;
+    list<Command *> command_list;
+    list<Command *>::iterator next_command;
 
-    CommandManager(){
+    CommandManager()
+    {
         next_command = command_list.end();
     }
-    ~CommandManager(){
-        for(auto i = command_list.begin(); i != command_list.end(); i++){
+    ~CommandManager()
+    {
+        for (auto i = command_list.begin(); i != command_list.end(); i++)
+        {
             delete *i;
         }
     }
 };
 
 int CommandManager::instance_count = 0;
-CommandManager* CommandManager::instance_ptr = nullptr;
+CommandManager *CommandManager::instance_ptr = nullptr;
 
 #endif // COMMAND_MANAGER_H
